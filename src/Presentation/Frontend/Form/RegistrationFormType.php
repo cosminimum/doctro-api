1a27802b0723806ba51e08530d2f64fa5b296844
Cosmin Gheorghila <cosmin@gheorghila.com>
2025-02-25 10:56:03 +0200

    Remove password constraints

--- src/Presentation/Frontend/Form/RegistrationFormType.php
+++ src/Presentation/Frontend/Form/RegistrationFormType.php
@@ -24,17 +24,7 @@ class RegistrationFormType extends AbstractType
             ->add('phone')
             ->add('plainPassword', PasswordType::class, [
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        'max' => 4096,
-                    ]),
-                ],
+                'attr' => ['autocomplete' => 'new-password']
             ])
         ;
     }
