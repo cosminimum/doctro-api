3d7d4892ff236cbb5912521257e8f90f571cc9dc
Cosmin Gheorghila <cosmin@gheorghila.com>
2025-03-29 10:27:08 +0200

    Fix sync slots

--- src/Command/SyncFhirResourcesCommand.php
+++ src/Command/SyncFhirResourcesCommand.php
@@ -706,6 +706,8 @@ class SyncFhirResourcesCommand extends Command
             }
             $this->output->writeln('Fetching slots from FHIR API for ' . $schedule->getIdHis());
             $response = $this->apiClient->get('/api/HInterop/GetSlots?schedule=' . $schedule->getIdHis());
+//            $response = '{"resourceType":"Bundle","type":"collection","entry":[{"fullUrl":"9497b700-5afb-4c76-9c4a-5a7e7c495707","resource":{"resourceType":"Slot","id":"1","identifier":[{"use":"official","type":{"coding":[{"system":"http://snomed.info/sct","code":"MR"}]},"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025__07:00__2__30"}],"serviceCategory":[{"coding":[{"code":"APPSERV"}],"text":"Servicii programari"}],"serviceType":[{"reference":{"type":"HealthcareService","identifier":{"system":"http://snomed.info/sct","value":"79831959-E760-435A-AC9A-775B27F8ACF2"}}}],"appointmentType":[{"coding":[{"code":"CHECKUP"}],"text":"CHECKUP"}],"schedule":{"type":"Schedule","identifier":{"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025"}},"status":"free","start":"2025-03-31T07:00:00+00:00","end":"2025-03-31T07:30:00+00:00"}},{"fullUrl":"5f030f83-5eda-4a25-b083-d6595f538f9c","resource":{"resourceType":"Slot","id":"2","identifier":[{"use":"official","type":{"coding":[{"system":"http://snomed.info/sct","code":"MR"}]},"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025__07:30__2__30"}],"serviceCategory":[{"coding":[{"code":"APPSERV"}],"text":"Servicii programari"}],"serviceType":[{"reference":{"type":"HealthcareService","identifier":{"system":"http://snomed.info/sct","value":"79831959-E760-435A-AC9A-775B27F8ACF2"}}}],"appointmentType":[{"coding":[{"code":"CHECKUP"}],"text":"CHECKUP"}],"schedule":{"type":"Schedule","identifier":{"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025"}},"status":"free","start":"2025-03-31T07:30:00+00:00","end":"2025-03-31T08:00:00+00:00"}},{"fullUrl":"2d782d6e-fbea-45d0-8dfd-5cfe7d567103","resource":{"resourceType":"Slot","id":"3","identifier":[{"use":"official","type":{"coding":[{"system":"http://snomed.info/sct","code":"MR"}]},"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025__08:00__2__30"}],"serviceCategory":[{"coding":[{"code":"APPSERV"}],"text":"Servicii programari"}],"serviceType":[{"reference":{"type":"HealthcareService","identifier":{"system":"http://snomed.info/sct","value":"79831959-E760-435A-AC9A-775B27F8ACF2"}}}],"appointmentType":[{"coding":[{"code":"CHECKUP"}],"text":"CHECKUP"}],"schedule":{"type":"Schedule","identifier":{"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025"}},"status":"free","start":"2025-03-31T08:00:00+00:00","end":"2025-03-31T08:30:00+00:00"}},{"fullUrl":"d859f91e-2533-4e66-b2a2-c728b7ff0ba9","resource":{"resourceType":"Slot","id":"4","identifier":[{"use":"official","type":{"coding":[{"system":"http://snomed.info/sct","code":"MR"}]},"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025__08:30__2__30"}],"serviceCategory":[{"coding":[{"code":"APPSERV"}],"text":"Servicii programari"}],"serviceType":[{"reference":{"type":"HealthcareService","identifier":{"system":"http://snomed.info/sct","value":"79831959-E760-435A-AC9A-775B27F8ACF2"}}}],"appointmentType":[{"coding":[{"code":"CHECKUP"}],"text":"CHECKUP"}],"schedule":{"type":"Schedule","identifier":{"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025"}},"status":"free","start":"2025-03-31T08:30:00+00:00","end":"2025-03-31T09:00:00+00:00"}},{"fullUrl":"457b75a5-0597-41da-bba3-d5e6bbed3524","resource":{"resourceType":"Slot","id":"5","identifier":[{"use":"official","type":{"coding":[{"system":"http://snomed.info/sct","code":"MR"}]},"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025__09:00__2__30"}],"serviceCategory":[{"coding":[{"code":"APPSERV"}],"text":"Servicii programari"}],"serviceType":[{"reference":{"type":"HealthcareService","identifier":{"system":"http://snomed.info/sct","value":"79831959-E760-435A-AC9A-775B27F8ACF2"}}}],"appointmentType":[{"coding":[{"code":"CHECKUP"}],"text":"CHECKUP"}],"schedule":{"type":"Schedule","identifier":{"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025"}},"status":"free","start":"2025-03-31T09:00:00+00:00","end":"2025-03-31T09:30:00+00:00"}},{"fullUrl":"69e524e3-fcce-4fb8-87c1-e79d4f332d9d","resource":{"resourceType":"Slot","id":"6","identifier":[{"use":"official","type":{"coding":[{"system":"http://snomed.info/sct","code":"MR"}]},"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025__09:30__2__30"}],"serviceCategory":[{"coding":[{"code":"APPSERV"}],"text":"Servicii programari"}],"serviceType":[{"reference":{"type":"HealthcareService","identifier":{"system":"http://snomed.info/sct","value":"79831959-E760-435A-AC9A-775B27F8ACF2"}}}],"appointmentType":[{"coding":[{"code":"CHECKUP"}],"text":"CHECKUP"}],"schedule":{"type":"Schedule","identifier":{"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025"}},"status":"free","start":"2025-03-31T09:30:00+00:00","end":"2025-03-31T10:00:00+00:00"}},{"fullUrl":"05b25e8d-2109-41db-b261-79582d4cb983","resource":{"resourceType":"Slot","id":"7","identifier":[{"use":"official","type":{"coding":[{"system":"http://snomed.info/sct","code":"MR"}]},"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025__10:00__2__30"}],"serviceCategory":[{"coding":[{"code":"APPSERV"}],"text":"Servicii programari"}],"serviceType":[{"reference":{"type":"HealthcareService","identifier":{"system":"http://snomed.info/sct","value":"79831959-E760-435A-AC9A-775B27F8ACF2"}}}],"appointmentType":[{"coding":[{"code":"CHECKUP"}],"text":"CHECKUP"}],"schedule":{"type":"Schedule","identifier":{"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025"}},"status":"free","start":"2025-03-31T10:00:00+00:00","end":"2025-03-31T10:30:00+00:00"}},{"fullUrl":"cfa02d9a-79a0-4839-89fa-0158d5317bff","resource":{"resourceType":"Slot","id":"8","identifier":[{"use":"official","type":{"coding":[{"system":"http://snomed.info/sct","code":"MR"}]},"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025__10:30__2__30"}],"serviceCategory":[{"coding":[{"code":"APPSERV"}],"text":"Servicii programari"}],"serviceType":[{"reference":{"type":"HealthcareService","identifier":{"system":"http://snomed.info/sct","value":"79831959-E760-435A-AC9A-775B27F8ACF2"}}}],"appointmentType":[{"coding":[{"code":"CHECKUP"}],"text":"CHECKUP"}],"schedule":{"type":"Schedule","identifier":{"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025"}},"status":"free","start":"2025-03-31T10:30:00+00:00","end":"2025-03-31T11:00:00+00:00"}},{"fullUrl":"b94ea1ef-44fc-4687-b5c2-a1da0137352e","resource":{"resourceType":"Slot","id":"9","identifier":[{"use":"official","type":{"coding":[{"system":"http://snomed.info/sct","code":"MR"}]},"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025__11:00__2__30"}],"serviceCategory":[{"coding":[{"code":"APPSERV"}],"text":"Servicii programari"}],"serviceType":[{"reference":{"type":"HealthcareService","identifier":{"system":"http://snomed.info/sct","value":"79831959-E760-435A-AC9A-775B27F8ACF2"}}}],"appointmentType":[{"coding":[{"code":"CHECKUP"}],"text":"CHECKUP"}],"schedule":{"type":"Schedule","identifier":{"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025"}},"status":"free","start":"2025-03-31T11:00:00+00:00","end":"2025-03-31T11:30:00+00:00"}},{"fullUrl":"4a5e4ad2-34cc-4b2a-847f-12733c1d8d4e","resource":{"resourceType":"Slot","id":"10","identifier":[{"use":"official","type":{"coding":[{"system":"http://snomed.info/sct","code":"MR"}]},"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025__11:30__2__30"}],"serviceCategory":[{"coding":[{"code":"APPSERV"}],"text":"Servicii programari"}],"serviceType":[{"reference":{"type":"HealthcareService","identifier":{"system":"http://snomed.info/sct","value":"79831959-E760-435A-AC9A-775B27F8ACF2"}}}],"appointmentType":[{"coding":[{"code":"CHECKUP"}],"text":"CHECKUP"}],"schedule":{"type":"Schedule","identifier":{"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025"}},"status":"free","start":"2025-03-31T11:30:00+00:00","end":"2025-03-31T12:00:00+00:00"}},{"fullUrl":"b8351e7c-88e0-4f9b-ab49-7c3d0a3ddafe","resource":{"resourceType":"Slot","id":"11","identifier":[{"use":"official","type":{"coding":[{"system":"http://snomed.info/sct","code":"MR"}]},"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025__12:00__2__30"}],"serviceCategory":[{"coding":[{"code":"APPSERV"}],"text":"Servicii programari"}],"serviceType":[{"reference":{"type":"HealthcareService","identifier":{"system":"http://snomed.info/sct","value":"79831959-E760-435A-AC9A-775B27F8ACF2"}}}],"appointmentType":[{"coding":[{"code":"CHECKUP"}],"text":"CHECKUP"}],"schedule":{"type":"Schedule","identifier":{"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025"}},"status":"free","start":"2025-03-31T12:00:00+00:00","end":"2025-03-31T12:30:00+00:00"}},{"fullUrl":"fc0c731a-cc12-4673-8451-8ef43e70aece","resource":{"resourceType":"Slot","id":"12","identifier":[{"use":"official","type":{"coding":[{"system":"http://snomed.info/sct","code":"MR"}]},"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025__12:30__2__30"}],"serviceCategory":[{"coding":[{"code":"APPSERV"}],"text":"Servicii programari"}],"serviceType":[{"reference":{"type":"HealthcareService","identifier":{"system":"http://snomed.info/sct","value":"79831959-E760-435A-AC9A-775B27F8ACF2"}}}],"appointmentType":[{"coding":[{"code":"CHECKUP"}],"text":"CHECKUP"}],"schedule":{"type":"Schedule","identifier":{"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025"}},"status":"free","start":"2025-03-31T12:30:00+00:00","end":"2025-03-31T13:00:00+00:00"}},{"fullUrl":"87ddf50b-c3b1-4cdc-aa96-eef4ebf0802c","resource":{"resourceType":"Slot","id":"13","identifier":[{"use":"official","type":{"coding":[{"system":"http://snomed.info/sct","code":"MR"}]},"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025__13:00__2__30"}],"serviceCategory":[{"coding":[{"code":"APPSERV"}],"text":"Servicii programari"}],"serviceType":[{"reference":{"type":"HealthcareService","identifier":{"system":"http://snomed.info/sct","value":"79831959-E760-435A-AC9A-775B27F8ACF2"}}}],"appointmentType":[{"coding":[{"code":"CHECKUP"}],"text":"CHECKUP"}],"schedule":{"type":"Schedule","identifier":{"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025"}},"status":"free","start":"2025-03-31T13:00:00+00:00","end":"2025-03-31T13:30:00+00:00"}},{"fullUrl":"098f2508-086a-46e6-93fc-72aa0a695499","resource":{"resourceType":"Slot","id":"14","identifier":[{"use":"official","type":{"coding":[{"system":"http://snomed.info/sct","code":"MR"}]},"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025__13:30__2__30"}],"serviceCategory":[{"coding":[{"code":"APPSERV"}],"text":"Servicii programari"}],"serviceType":[{"reference":{"type":"HealthcareService","identifier":{"system":"http://snomed.info/sct","value":"79831959-E760-435A-AC9A-775B27F8ACF2"}}}],"appointmentType":[{"coding":[{"code":"CHECKUP"}],"text":"CHECKUP"}],"schedule":{"type":"Schedule","identifier":{"system":"http://snomed.info/sct","value":"1__422900000000504__31_03_2025"}},"status":"free","start":"2025-03-31T13:30:00+00:00","end":"2025-03-31T14:00:00+00:00"}}]}';
+//            $response = json_decode($response,true);
 
             $stats = [
                 'total'   => count($response['entry'] ?? []),
@@ -733,7 +735,7 @@ class SyncFhirResourcesCommand extends Command
                     }
 
                     $slotResource = $entry['resource'];
-                    $hisId        = $slotResource['id'] ?? null;
+                    $hisId = $slotResource['identifier']['value'] ?? null;
 
                     if (!$hisId) {
                         $this->output->writeln('Skipping Slot without ID');
@@ -742,9 +744,7 @@ class SyncFhirResourcesCommand extends Command
                     }
 
                     // Check if we already have this slot
-                    $existingSlot
-                        = $this->entityManager->getRepository(TimeSlot::class)
-                        ->findOneBy(['idHis' => $hisId]);
+                    $existingSlot = $this->entityManager->getRepository(TimeSlot::class)->findOneBy(['idHis' => $hisId]);
 
                     $this->output->writeln("Slot ID: {$hisId} - Status: {$slotResource['status']} - {$slotResource['start']} | {$slotResource['end']}");
                     // Extract basic slot information
@@ -766,10 +766,7 @@ class SyncFhirResourcesCommand extends Command
                         $slotDate->setTime(0, 0, 0);
                     } else {
                         if (!$existingSlot) {
-                            $this->output->writeln('Slot missing start/end times, cannot create new slot',
-                                [
-                                    'slotId' => $hisId
-                                ]);
+                            $this->output->writeln('Slot missing start/end times, cannot create new slot');
                             $stats['skipped']++;
                             continue;
                         }
@@ -778,15 +775,11 @@ class SyncFhirResourcesCommand extends Command
                     // Get hospital service if available
                     $hospitalService = null;
                     if (isset($slotResource['serviceType'][0]['reference']['identifier']['value'])) {
-                        $serviceHisId
-                            = $slotResource['serviceType'][0]['reference']['identifier']['value'];
-                        $hospitalService
-                            = $this->entityManager->getRepository(HospitalService::class)
-                            ->findOneBy(['idHis' => $serviceHisId]);
+                        $serviceHisId = $slotResource['serviceType'][0]['reference']['identifier']['value'];
+                        $hospitalService = $this->hospitalServiceRepository->findOneBy(['id' => $serviceHisId]);
                     }
 
                     if ($existingSlot) {
-                        // Update existing slot
                         $existingSlot->setIsBooked($isBooked);
 
                         if ($status) {
@@ -821,67 +814,7 @@ class SyncFhirResourcesCommand extends Command
                         $this->entityManager->flush();
                         $stats['updated']++;
                     } else {
-                        // We need to create a new slot - first find or create the schedule
-                        $scheduleRef
-                            = $slotResource['schedule']['identifier']['value']
-                            ?? null;
-
-                        if (!$scheduleRef) {
-                            $this->output->writeln('Slot missing schedule reference, cannot create new slot',
-                                [
-                                    'slotId' => $hisId
-                                ]);
-                            $stats['skipped']++;
-                            continue;
-                        }
-
-                        // Find existing schedule
-                        $schedule
-                            = $this->entityManager->getRepository(DoctorSchedule::class)
-                            ->findOneBy(['idHis' => $scheduleRef]);
-
-                        if (!$schedule) {
-                            // Need to create a new schedule - extract doctor information from reference
-                            // Assuming schedule ID has format like "2__422900000000080__26_03_2025__10:00__11__30"
-                            $identifierParts = explode('__', $scheduleRef);
-
-                            if (count($identifierParts) < 3) {
-                                $this->output->writeln('Cannot parse doctor ID from schedule reference',
-                                    [
-                                        'scheduleRef' => $scheduleRef,
-                                        'slotId'      => $hisId
-                                    ]);
-                                $stats['skipped']++;
-                                continue;
-                            }
-
-                            $doctorHisId = $identifierParts[1] ?? null;
-
-                            $doctor
-                                = $this->entityManager->getRepository(Doctor::class)
-                                ->findOneBy(['idHis' => $doctorHisId]);
-
-                            if (!$doctor) {
-                                $this->output->writeln('Doctor not found for this slot',
-                                    [
-                                        'doctorHisId' => $doctorHisId,
-                                        'slotId'      => $hisId
-                                    ]);
-                                $stats['skipped']++;
-                                continue;
-                            }
-
-                            // Create the schedule
-                            $schedule = new DoctorSchedule();
-                            $schedule->setIdHis($scheduleRef);
-                            $schedule->setDoctor($doctor);
-                            $schedule->setDate($slotDate);
-
-                            $this->entityManager->persist($schedule);
-                            $this->entityManager->flush();
-                        }
-
-                        // Now create the time slot
+                        $schedule = $this->doctorScheduleRepository->findOneBy(['idHis' => $schedule->getIdHis()]);
                         $newSlot = new TimeSlot();
                         $newSlot->setIdHis($hisId);
                         $newSlot->setSchedule($schedule);
@@ -891,7 +824,6 @@ class SyncFhirResourcesCommand extends Command
                             $newSlot->setStatus($status);
                         }
 
-                        // Create time-only objects for the slot
                         $startTime = new \DateTime();
                         $startTime->setTime(
                             (int)$startDateTime->format('H'),
